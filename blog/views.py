0d78d369a22a1984c4ca5454f37d1646ee386c07
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-07-08 21:18:23 +0400

    add style css

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render, HttpResponse
 
 
+posts = [
+    {
+        "author": "ASGAR ISMAYILOV",
+        "title": "Blog Post 1",
+        "content": "This is my first blog post",
+        "date_posted": "7th August, 2024",
+    },
+    {
+        "author": "Destiny Franks",
+        "title": "Blog Post 2",
+        "content": "This is my second blog post",
+        "date_posted": "14th August, 2024",
+    },
+]
+
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1> Welcome My home </h1>")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1> Welcome My about </h1>")
+    return render(request, "blog/about.html")
