3de69cfc075953d109dcc630a2de697b3e0d7170
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-07-09 00:00:45 +0400

    sace

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from blog.models import Post
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["author", "title"]
+    list_display_links = ["author"]
+    search_fields = ["auhtor", "title", "content"]
+    list_filter = ["date_posted", "title"]
+
+    class Meta:
+        model = Post
+
+
+# old way
+# admin.site.register(Post, PostAdmin)
